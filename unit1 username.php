1aae36b08231cd2b711b42aabf597fd1154ffd3e
taufik749 <taufikaehmad.badi128552@marwadiuniversity.ac.in>
2026-06-11 09:39:28 +0530

    Add files via upload

---
+++ unit1 username.php
@@ -0,0 +1,12 @@
+<?php
+    error_reporting(0);
+?>
+
+<form method="get">
+        <input type="text" name="uname>
+        <input type="submit" name="submit"value"submit">
+</form>
+
+<?php
+        $nm = GET['uname'];
+        echo"hello, " .$nm;
